596a8e609b932256ac231a76b81534d2bbb1472c
Aakrish Lama <al226na@student.lnu.se>
2025-08-26 12:33:42 +0200

    orbitcontrol through a loop function

--- threeLessons/src/main.js
+++ threeLessons/src/main.js
@@ -6,7 +6,7 @@ const scene = new THREE.Scene();
 
 // create cube
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial  = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const cubeMaterial  = new THREE.MeshBasicMaterial({color: "red", wireframe: true});
 
 const cube = new THREE.Mesh(
     cubeGeometry, cubeMaterial
@@ -25,10 +25,25 @@ scene.add(camera);
 
 // initialize renderer
 const canvas = document.querySelector(".threejs")
-const renderer = new THREE.WebGLRenderer({canvas: canvas});
-
-// intitialize the controls 
-const controls = new OrbitControls(camera, canvas);
-
+const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
+
+
+// intitialize the controls for orbit.
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping= true;
+controls.enableRotate= true;
+controls.enableZoom= true;
+
+
+// Animation loop: continuously renders the scene from the camera's perspective,
+// updates the orbit controls for smooth interaction, and requests the next frame.
+const renderloop = () => {
+    renderer.render(scene, camera);
+    controls.update();
+    window.requestAnimationFrame(renderloop);
+}
+
+renderloop();
+
+
