13e9b32ac1caf725a1e28b590c9f358d1cd9f4c9
Aakrish Lama <al226na@student.lnu.se>
2025-08-26 13:40:35 +0200

    fix the resize for the window size

--- threeLessons/src/main.js
+++ threeLessons/src/main.js
@@ -29,19 +29,30 @@ const renderer = new THREE.WebGLRenderer({canvas: canvas, antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 
+
+
 // intitialize the controls for orbit.
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping= true;
-controls.enableRotate= true;
+// controls.enableRotate= true;
 controls.enableZoom= true;
 
+// resizing the camera aspect and window size in the browser.
+window.addEventListener("resize", ()=>{
+    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+})
 
 // Animation loop: continuously renders the scene from the camera's perspective,
 // updates the orbit controls for smooth interaction, and requests the next frame.
 const renderloop = () => {
     renderer.render(scene, camera);
     controls.update();
+    cube.rotation.y += 0.01;
     window.requestAnimationFrame(renderloop);
+
 }
 
 renderloop();
