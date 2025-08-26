5be7375f6b15d4eb8e89d5f83d7c7ced81101b09
Aakrish Lama <al226na@student.lnu.se>
2025-08-26 11:28:12 +0200

    rendering a cube

--- threeLessons/src/main.js
+++ threeLessons/src/main.js
@@ -1,7 +1,9 @@
 import * as THREE from "three";
 
+// initialize scene
 const scene = new THREE.Scene();
 
+// create cube
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 const cubeMaterial  = new THREE.MeshBasicMaterial({color: 0x00ff00});
 
@@ -10,4 +12,15 @@ const cube = new THREE.Mesh(
 );
 
 scene.add(cube);
-console.log(scene);
+
+// camera
+console.log(window.innerHeight, window.innerWidth);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 30);
+camera.position.z = 5;
+scene.add(camera);
+
+// initialize renderer
+const canvas = document.querySelector(".threejs")
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.render(scene, camera);
