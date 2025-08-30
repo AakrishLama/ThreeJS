1f4f238aa3e15b7abaeaec158b924e5e19f09992
Aakrish Lama <al226na@student.lnu.se>
2025-08-30 17:36:20 +0200

    adding moon to each of the planet meshes

--- threeLessons/src/planets.js
+++ threeLessons/src/planets.js
@@ -8,7 +8,15 @@ const planets = [{
     size: 0.5,
     positionX: 2,
     positionY: 0,
-    positionZ: 0
+    positionZ: 0,
+    moons: [{
+        name: "moon",
+        color: 0x888888,
+        size: 0.1,
+        positionX: 1,
+        positionY: 0,
+        positionZ: 0
+    }]
 },
 {
     name: "mars",
@@ -16,7 +24,23 @@ const planets = [{
     size: 0.3,
     positionX: 4,
     positionY: 0,
-    positionZ: 0
+    positionZ: 0,
+    moons: [{
+        name: "phobos",
+        color: 0xaaaaaa,
+        size: 0.05,
+        positionX: 0.5,
+        positionY: 0,
+        positionZ: 0
+    },
+    {
+        name: "deimos",
+        color: 0xcccccc,
+        size: 0.03,
+        positionX: 0.8,
+        positionY: 0,
+        positionZ: 0
+    }]
 },
 {
     name: "venus",
@@ -24,7 +48,8 @@ const planets = [{
     size: 0.4,
     positionX: 6,
     positionY: 0,
-    positionZ: 0
+    positionZ: 0,
+    moons: []
 },
 {
     name: "jupiter",
@@ -32,14 +57,27 @@ const planets = [{
     size: 0.7,
     positionX: 8,
     positionY: 0,
-    positionZ: 0
+    positionZ: 0,
+    moons: []
 }];
 
+// create planet meshes.
 const planetMeshes = planets.map((planet)=>{
     const geometry = new THREE.SphereGeometry(planet.size, 32, 10);
     const material = new THREE.MeshStandardMaterial({ color: planet.color , wireframe: true });
     const mesh = new THREE.Mesh(geometry, material);
     mesh.position.set(planet.positionX, planet.positionY, planet.positionZ);
+    // create moons for each planet.
+    planet.moons.forEach((moon)=>{
+        const moonGeometry = new THREE.SphereGeometry(moon.size, 32, 10);
+        const moonMaterial = new THREE.MeshStandardMaterial({ color: moon.color , wireframe: true });
+        const moonMesh = new THREE.Mesh(moonGeometry, moonMaterial);
+        moonMesh.position.set(moon.positionX, moon.positionY, moon.positionZ);
+        mesh.add(moonMesh);
+    })
     return mesh;
 })
+
+
+// export the planet meshes.
 export default planetMeshes; 
